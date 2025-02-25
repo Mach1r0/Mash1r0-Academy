4a7ee207eed19e0275dbb8bdf558a9ce2dd3413f
Mach1r0 <segundacontafd@gmail.com>
2025-02-24 22:01:41 -0300

    feat: adding daisyui on project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        'background-color': 'var(--secondary-color)',
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
+
+export default config; 
